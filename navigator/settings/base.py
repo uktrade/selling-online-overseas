c68b222f31d0b66e0bfcdfb6d0d08b19d444dcad
David Downes <sourcetree@theukdave.com>
2016-08-23 14:40:33 +0100

    Fixed test environment script
    Added ckeditor
    
    removed cpm build from the yaml file as we don't have a build yet

--- navigator/settings/base.py
+++ navigator/settings/base.py
@@ -30,7 +30,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
+    'ckeditor'
 ]
 
 MIDDLEWARE_CLASSES = [
