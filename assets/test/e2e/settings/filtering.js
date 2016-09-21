a2a744b03b227b6862cfc02f3341ab1d142b4815
Gerald Isabelle <info@geraldisabelle.me>
2016-09-21 17:02:40 +0100

    create filters test

--- assets/test/e2e/settings/filtering.js
+++ assets/test/e2e/settings/filtering.js
@@ -2,5 +2,8 @@ module.exports = {
     results: element(by.css('#results-count h4')),
     resultsButton: element(by.css('.button-result')),
     sellerModelOption: element(by.css('#id_seller_model_0')),
-    listHeaderResults: element(by.css('.heading-results'))
+    productTypeModelOption: element(by.css('#id_product_type_0')),
+    listHeaderResults: element(by.css('.heading-results')),
+    countryList: element(by.id('countries_served')).all(by.tagName('option')),
+    productList: element(by.id('product_categories')).all(by.tagName('option'))
 };
