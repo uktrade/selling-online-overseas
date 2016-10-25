4fa7929b2632e5444ccfd64e26bd16bc1ffae7e5
David Downes <me@theukdave.com>
2016-10-25 14:26:57 +0100

    still trying to get static files serving

--- navigator/settings/test.py
+++ navigator/settings/test.py
@@ -4,8 +4,3 @@ DEBUG = True
 ALLOWED_HOSTS = ['*.herokuapp.com']
 
 WHOOSH_INDEX_DIR = os.path.join(BASE_DIR, 'whoosh_index_test')
-
-INSTALLED_APPS += [
-    'whitenoise.runserver_nostatic',
-    'django.contrib.staticfiles',
-]
