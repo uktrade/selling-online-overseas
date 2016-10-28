a006da4a03968b9f6b7158fb857e65317e8e9640
David Downes <me@theukdave.com>
2016-10-28 11:25:37 +0100

    Turn off ip checking

--- navigator/settings/prod.py
+++ navigator/settings/prod.py
@@ -4,7 +4,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 ADMINS = (('David Downes', 'david@downes.co.uk'),)
 
-RESTRICT_IPS = True
+RESTRICT_IPS = False
 ALLOWED_IPS = ['127.0.0.1', '94.119.64.50']
 ALLOWED_IP_RANGES = ['165.225.80.0/22', '193.240.203.32/29', '94.119.64.0/24', '178.208.163.0/24']
 BLOCKED_IPS = []
