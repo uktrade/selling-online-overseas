546db5e01df45325346472d83181c11eef8d0797
David Downes <me@theukdave.com>
2016-10-28 10:59:53 +0100

    Fixed IP checking

--- core/middleware.py
+++ core/middleware.py
@@ -14,20 +14,25 @@ class IpRestrictionMiddleware(object):
 
     def process_request(self, request):
         if getattr(settings, 'RESTRICT_IPS', False):
-            print(request.META['REMOTE_ADDR'])
-            if request.META['REMOTE_ADDR'] not in settings.ALLOWED_IPS:
-                return http.HttpResponseForbidden('<h1>Forbidden</h1>')
-            if request.META['REMOTE_ADDR'] in settings.BLOCKED_IPS:
-                return http.HttpResponseForbidden('<h1>Forbidden</h1>')
 
+            block_request = True
             request_ip = ipaddress.ip_address(request.META['REMOTE_ADDR'])
 
+            if request.META['REMOTE_ADDR'] in settings.ALLOWED_IPS:
+                block_request = False
+
             for allowed_range in ALLOWED_IP_RANGES:
-                if request_ip not in ipaddress.ip_network(allowed_range):
-                    return http.HttpResponseForbidden('<h1>Forbidden</h1>')
+                if request_ip in ipaddress.ip_network(allowed_range):
+                    block_request = False
+
+            if request.META['REMOTE_ADDR'] in settings.BLOCKED_IPS:
+                block_request = True
 
             for blocked_range in BLOCKED_IP_RANGES:
                 if request_ip in ipaddress.ip_network(blocked_range):
-                    return http.HttpResponseForbidden('<h1>Forbidden</h1>')
+                    block_request = True
+
+            if block_request:
+                return http.HttpResponseForbidden('<h1>Forbidden</h1>')
 
         return None
