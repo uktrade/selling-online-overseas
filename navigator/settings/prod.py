cdebdc545137a7f6dbdd5a0aef3976ea27699d65
David Downes <me@theukdave.com>
2016-11-02 08:48:12 +0000

    redirect requests to http to https using the new django setting

--- navigator/settings/prod.py
+++ navigator/settings/prod.py
@@ -6,3 +6,5 @@ ADMINS = (('David Downes', 'david@downes.co.uk'),)
 
 ALLOWED_IPS = []
 ALLOWED_IP_RANGES = ['165.225.80.0/22', '193.240.203.32/29', '94.119.64.0/24', '178.208.163.0/24']
+
+SECURE_SSL_REDIRECT = True
