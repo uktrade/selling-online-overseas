533c7ec698c196bf8b7eb16ed3fc7ce60f3afac4
David Downes <me@theukdave.com>
2016-11-02 08:45:35 +0000

    create a new staging settings file remove the ip middleware from base, and add it in the relevant settings fix bug with ip from heroku, by stripping the string

--- core/middleware.py
+++ core/middleware.py
@@ -19,7 +19,7 @@ class IpRestrictionMiddleware(object):
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
 
         if x_forwarded_for:
-            ip = x_forwarded_for.split(',')[-1]
+            ip = x_forwarded_for.split(',')[-1].strip()
         else:
             ip = request.META.get('REMOTE_ADDR')
 
