f5a856b6c6c64ff8bba56a04a25bb639e98ff0e2
David Downes <me@theukdave.com>
2016-11-02 12:45:03 +0000

    Add locust testing box to allowed IPs, and get the SSL redirect from the environment so that it can be turned off for load testing
    pep8

--- navigator/settings/prod.py
+++ navigator/settings/prod.py
@@ -19,7 +19,8 @@ RAVEN_CONFIG = {
 ip_check = os.environ.get('RESTRICT_IPS', False)
 RESTRICT_IPS = ip_check == 'True' or ip_check == '1'
 
-ALLOWED_IPS = []
+ALLOWED_IPS = ['54.229.170.70']
 ALLOWED_IP_RANGES = ['165.225.80.0/22', '193.240.203.32/29', '94.119.64.0/24', '178.208.163.0/24']
 
-SECURE_SSL_REDIRECT = True
+ssl_check = os.environ.get('SSL_REDIRECT', False)
+SECURE_SSL_REDIRECT = ssl_check == 'True' or ssl_check == '1'
