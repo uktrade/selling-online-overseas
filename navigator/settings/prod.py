54c90c327569819d57e7437deabb3362685bbfdc
David Downes <me@theukdave.com>
2016-11-03 11:46:45 +0000

    my home ip

--- navigator/settings/prod.py
+++ navigator/settings/prod.py
@@ -19,7 +19,7 @@ RAVEN_CONFIG = {
 ip_check = os.environ.get('RESTRICT_IPS', False)
 RESTRICT_IPS = ip_check == 'True' or ip_check == '1'
 
-ALLOWED_IPS = ['54.229.170.70', '82.35.63.118']
+ALLOWED_IPS = ['54.229.170.70', '82.35.63.118', '52.49.20.243']
 ALLOWED_IP_RANGES = ['165.225.80.0/22', '193.240.203.32/29', '94.119.64.0/24', '178.208.163.0/24']
 
 SECURE_SSL_REDIRECT = True
