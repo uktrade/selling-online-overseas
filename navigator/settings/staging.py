d6c63e2d5341ff006aa6495f84e7e264e9d3987d
David Downes <me@theukdave.com>
2016-11-07 13:09:57 +0000

    fix allowed IPs on staging

--- navigator/settings/staging.py
+++ navigator/settings/staging.py
@@ -12,4 +12,4 @@ ip_check = os.environ.get('RESTRICT_IPS', False)
 RESTRICT_IPS = ip_check == 'True' or ip_check == '1'
 
 ALLOWED_IPS = []
-ALLOWED_IP_RANGES = ['165.225.80.0/22', '193.240.203.32/29', '94.119.64.0/24', '178.208.163.0/24']
+ALLOWED_IP_RANGES = ['165.225.80.0/22', '193.240.203.32/29']
