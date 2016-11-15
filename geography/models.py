ca8d93651134ddf7eec49f763b4118d06b7782a3
David Downes <me@theukdave.com>
2016-11-15 11:12:42 +0000

    Add an alternate country name to be searchable on

--- geography/models.py
+++ geography/models.py
@@ -13,10 +13,18 @@ class Region(models.Model):
 
 class Country(models.Model):
     name = models.CharField(max_length=200, unique=True)
+    alternate_name = models.CharField(max_length=200, null=True, blank=True)
     region = models.ForeignKey(Region)
 
     def __str__(self):
         return "{0}".format(self.name)
+
+    @property
+    def verbose_name(self):
+        if self.alternate_name:
+            return "{0} ({1})".format(self.name, self.alternate_name)
+        else:
+            return self.__str__()
 
     class Meta:
         verbose_name_plural = "Countries"
