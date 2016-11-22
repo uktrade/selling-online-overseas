da2ca7a509dd6164d061e8a7e705093593809f0b
David Downes <me@theukdave.com>
2016-11-22 12:30:36 +0000

    Omit the mature category on ministerial orders, to reduce the risk of reputational damage Updated the dev settings since it doesn't run on SSL Set the session cookies to expire after 12 hours forcing a re-login on admin

--- products/management/commands/build_index.py
+++ products/management/commands/build_index.py
@@ -40,6 +40,10 @@ class Command(BaseCommand):
             # Get the top-level category name for the row
             category = row[1]
 
+            if category == 'Mature':
+                # Omit the mature category on ministerial orders, to reduce the risk of reputational damage
+                continue
+
             # Go over all columns in the row
             for index in range(2, len(row)):
                 # When you find an empty column, the PREVIOUS column was a leaf sub-category for the main category
