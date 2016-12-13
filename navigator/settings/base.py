971fcb76b2291c6be29629c40839c4b571fe8425
David Downes <me@theukdave.com>
2016-12-13 16:54:21 +0000

    Remove existing publishing, and add in django-reversion
    Get publishing workflow working using a custom url on the Admin object
    Restrict publishing to a new permission
    Update migrations for all the new stuff, including publishing already-published markets
    
    Removed double-requirement
    
    updated requirements
    
    Highlight changed items between a Market and the related PublishedMarket
    
    Fix some styles on the history page, and add a publish button to the footer
    
    Save the market before publishing if the user clicks publish
    Hide the publish and delete buttons if the user doesn't have permissions for those action

--- navigator/settings/base.py
+++ navigator/settings/base.py
@@ -26,6 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 INSTALLED_APPS = [
     'grappelli',
+    'reversion',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
