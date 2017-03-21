4ea77215973e31adc847c319a80db917510befd1
David Downes <me@theukdave.com>
2017-03-21 16:47:29 +0000

    New dedicated pingdom view

--- navigator/urls.py
+++ navigator/urls.py
@@ -5,9 +5,12 @@ from django.conf.urls.static import static
 from django.views.generic import RedirectView, TemplateView
 from django.core.urlresolvers import reverse_lazy
 
+from core.views import PingView
+
 
 urlpatterns = [
     url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    url(r'^ping\.json$', PingView.as_view(), name='ping'),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^$', RedirectView.as_view(url=reverse_lazy('markets:home')), name="home"),
