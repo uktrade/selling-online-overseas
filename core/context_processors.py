0aeb27717e036360b9797817dace711f849c307b
David Downes <me@theukdave.com>
2017-03-30 15:19:47 +0100

    Fix the context processor with the PROFILE_HOST Update to security patched django and pillow versions

--- core/context_processors.py
+++ core/context_processors.py
@@ -6,6 +6,7 @@ def hosts(request):
         'SOO_HOST': settings.SOO_HOST,
         'HELP_HOST': settings.HELP_HOST,
         'SSO_HOST': settings.SSO_HOST,
+        'PROFILE_HOST': settings.PROFILE_HOST
     }
 
     return hosts
