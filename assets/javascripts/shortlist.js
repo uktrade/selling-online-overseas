fe71dc78f8b2d39cacc9d19796e375d38799a733
Gerald Isabelle <info@geraldisabelle.me>
2017-08-14 16:45:50 +0100

    add icons and ajax call to shortlist

--- assets/javascripts/shortlist.js
+++ assets/javascripts/shortlist.js
@@ -1,10 +1,39 @@
 var sortList = (function () {
 
+    var shortlist = $('.markets-shortlist');
+
+    shortlist.click(shortList);
 
     function init() {
 
     }
 
+
+    function shortList(event) {
+        event.preventDefault();
+
+        var slug = $(event.target).data('slug');
+
+
+        if($(event.target).hasClass('markets-shortlist--shortlisted')) {
+            $.ajax({
+                url: '/markets/api/shortlist/remove/'+slug,
+                type: 'GET',
+                success:function(result) {
+                    console.log('remove to shortlist');
+                }
+            });
+        } else {
+            $.ajax({
+                url: '/markets/api/shortlist/add/'+slug,
+                type: 'GET',
+                success:function(result) {
+                    console.log('added to shortlist');
+                }
+            });
+        }
+
+    }
 
     return {
         init: init
@@ -12,4 +41,4 @@ var sortList = (function () {
 
 }(jQuery));
 
-sorting.init();
+sortList.init();
