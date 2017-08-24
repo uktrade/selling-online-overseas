cd76691f355fef216860df84ab5944165fb374d4
David Downes <me@theukdave.com>
2017-08-24 11:54:09 +0100

    allow heroku as a domain for staging whilst staging is moved to GDS PaaS

--- navigator/settings/staging.py
+++ navigator/settings/staging.py
@@ -1,7 +1,7 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['selling-online-overseas.export.staging.uktrade.io']
+ALLOWED_HOSTS = ['selling-online-overseas.export.staging.uktrade.io', 'dit-navigator-staging.herokuapp.com']
 ADMINS = (('David Downes', 'david@downes.co.uk'),)
 
 RESTRICT_IPS = True
