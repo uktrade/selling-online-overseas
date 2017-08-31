7408a7a4a09de5f2309376675168f6b24e35d398
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2017-08-31 23:42:27 +0100

    Fix ALLOWED_HOSTS for staging

--- app/navigator/settings/staging.py
+++ app/navigator/settings/staging.py
@@ -1,7 +1,10 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['selling-online-overseas.export.staging.uktrade.io', 'dit-navigator-staging.herokuapp.com', 'selling-online-overseas.export.staging.uktrade.io']
+ALLOWED_HOSTS = ['selling-online-overseas.export.staging.uktrade.io',
+                 'dit-navigator-staging.herokuapp.com',
+                 'navigator-staging.cloudapps.digital']
+
 ADMINS = (('David Downes', 'david@downes.co.uk'),)
 
 RESTRICT_IPS = True
