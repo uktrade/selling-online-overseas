8026fd4a1b680b2d72d3e6acfd413bf5edadf74a
eadpearce <e.pearce@tutanota.com>
2018-01-09 16:56:50 +0000

    Updated addresses to use soo.trade.great

--- app/navigator/settings/base.py
+++ app/navigator/settings/base.py
@@ -213,17 +213,17 @@ SSO_PROFILE_URL = os.environ.get(
 SSO_PROXY_SIGNATURE_SECRET = os.environ.get(
     'SSO_PROXY_SIGNATURE_SECRET', 'proxy_signature_debug')
 SSO_PROXY_API_CLIENT_BASE_URL = os.environ.get(
-    'SSO_PROXY_API_CLIENT_BASE_URL', 'http://sso.trade.great.dev:8004/')
+    'SSO_PROXY_API_CLIENT_BASE_URL', 'http://sso.trade.great:8004/')
 SSO_PROXY_LOGIN_URL = os.environ.get(
-    'SSO_PROXY_LOGIN_URL', 'http://sso.trade.great.dev:8004/accounts/login/')
+    'SSO_PROXY_LOGIN_URL', 'http://sso.trade.great:8004/accounts/login/')
 SSO_PROXY_LOGOUT_URL = os.environ.get(
-    'SSO_PROXY_LOGOUT_URL', 'http://sso.trade.great.dev:8004/accounts/'
-    'logout/?next=http://soo.trade.great.dev:8001')
+    'SSO_PROXY_LOGOUT_URL', 'http://sso.trade.great:8004/accounts/'
+    'logout/?next=http://soo.trade.great:8001')
 SSO_PROXY_SIGNUP_URL = os.environ.get(
-    'SSO_PROXY_SIGNUP_URL', 'http://sso.trade.great.dev:8004/accounts/signup/')
+    'SSO_PROXY_SIGNUP_URL', 'http://sso.trade.great:8004/accounts/signup/')
 SSO_PROFILE_URL = os.environ.get(
     'SSO_PROFILE_URL',
-    'http://profile.trade.great.dev:8006/selling-online-overseas/')
+    'http://profile.trade.great:8006/selling-online-overseas/')
 SSO_PROXY_REDIRECT_FIELD_NAME = os.environ.get(
     'SSO_PROXY_REDIRECT_FIELD_NAME', 'next')
 SSO_PROXY_SESSION_COOKIE = os.environ.get(
