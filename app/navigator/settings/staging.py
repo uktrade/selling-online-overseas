1b661fb7d2a1bbfe5eca063ed6cf70bec321db9c
Jayesh Patel <ejayesh@hotmail.com>
2019-01-10 10:22:54 +0000

    New London PaaS app urls required for migration

--- app/navigator/settings/staging.py
+++ app/navigator/settings/staging.py
@@ -5,7 +5,9 @@ DEBUG = False
 ALLOWED_HOSTS = ['selling-online-overseas.export.staging.uktrade.io',
                  'dit-navigator-staging.herokuapp.com',
                  'navigator-staging.cloudapps.digital',
-                 'enav-navigator-dev.cloudapps.digital']
+                 'enav-navigator-dev.cloudapps.digital',
+                 'navigator-dev.london.cloudapps.digital',
+                 'navigator-staging.london.cloudapps.digital']
 
 ADMINS = (('David Downes', 'david@downes.co.uk'),)
 
