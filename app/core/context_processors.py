6c19767391e0b9ca5b1130728823a6fe99660a0e
Will Taylor <wrftaylor@gmail.com>
2019-09-12 17:13:33 +0100

    rewind directory_components version

--- app/core/context_processors.py
+++ app/core/context_processors.py
@@ -4,6 +4,10 @@ from django.conf import settings
 
 def hosts(request):
     hosts = {
+        'SOO_HOST': urls.SERVICES_SOO,
+        'HELP_HOST': urls.CONTACT_US,
+        'SSO_HOST': urls.SERVICES_SSO,
+        'SERVICES_GREAT_DOMESTIC': urls.SERVICES_GREAT_DOMESTIC,
         'PROFILE_HOST': settings.PROFILE_HOST
     }
 
