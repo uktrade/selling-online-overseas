712e001094739e42e69777e813749c62883457a2
Rizwan Mansuri <rizwan@webbyfox.co.uk>
2021-03-23 11:43:07 +0000

    Pin django version to > 2.2

--- app/navigator/urls.py
+++ app/navigator/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from django.urls import include, path
+from django.urls import include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
